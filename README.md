f4fb0e825fdba2ef63616120f70c1fc44fedd2dd
Kathryn Killebrew <kkillebrew@azavea.com>
2020-08-05 15:41:20 -0400

    Configure MapBox navigation library
    Install packages for Mapbox navigation and intialize it in main activity.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # android-trip-recorder
+
 Using MapBox libraries to record and replay trips to test navigation
+
+## Requirements
+
+ - Android Studio
+ - Java 8
+ - Kotlin
+ - a MapBox account
+
+
+## Setup
+
+Set up a MapBox account. Create a secret access token, as described [here](https://docs.mapbox.com/android/beta/navigation/overview/#configure-credentials).
+
+Copy the example API keys file for the client into the app:
+
+`cp examples/api_keys.xml app/src/main/res/values/`
+
+Put the public token for your MapBox account into `app/src/main/res/values/api_keys.xml` as the value for the `mapbox_access_token` key.
+
+Create a local `gradle.properties` file in your user directory and add your secret MapBox token to it as the value for `MAPBOX_DOWNLOADS_TOKEN`, as described [here](https://docs.mapbox.com/android/beta/navigation/overview/#configure-your-secret-token).
--- app/src/main/java/com/banderkat/triprecorder/MainActivity.kt
+++ app/src/main/java/com/banderkat/triprecorder/MainActivity.kt
@@ -1,23 +1,69 @@
 package com.banderkat.triprecorder
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.navigation.core.MapboxNavigation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener {
 
+    private var recording = false
+    private var nav: MapboxNavigation? = null
+    private var permissionsManager: PermissionsManager? = null
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            Log.d("MainActivity", "permission already granted")
+            val options = MapboxNavigation.defaultNavigationOptionsBuilder(
+                this,
+                getString(R.string.mapbox_access_token)
+            ).build()
+            nav = MapboxNavigation(options)
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager?.requestLocationPermissions(this)
         }
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            recording = !recording
+
+            if (recording) nav?.startTripSession() else nav?.stopTripSession()
+            nav?.toggleHistory(recording)
+
+            var recordingString = if (recording) this.getString(R.string.start_recording)
+            else this.getString(R.string.stop_recording)
+
+            if (!recording) {
+                Log.d("MainActivity", "Stopped recording")
+                val history = nav?.retrieveHistory()
+                val historyString = if (history.isNullOrBlank()) "" else history.toString()
+                Log.d("MainActivity", historyString)
+                recordingString += ": "
+                recordingString += historyString
+            }
+
+            Snackbar.make(view, recordingString, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        permissionsManager?.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +79,24 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Log.d("MainActivity", "TODO: location explanation needed")
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            Log.d("MainActivity", "permission granted")
+            nav = MapboxNavigation(
+                MapboxNavigation.defaultNavigationOptionsBuilder(
+                    applicationContext,
+                    getString(R.string.mapbox_access_token)
+                ).build()
+            )
+        } else {
+            Log.d("MainActivity", "permission denied")
         }
     }
 }
